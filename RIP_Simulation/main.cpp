38e08e7dbc2c583f896f5b7d0c3793305bb7f6d4
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-11 00:42:38 -0800

    Add: Router Class and RoutingTableEntry struct.

--- RIP_Simulation/main.cpp
+++ RIP_Simulation/main.cpp
@@ -7,6 +7,58 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct RoutingTableEntry
+{
+    RoutingTableEntry()
+    {}
+    
+    RoutingTableEntry(string d, string n, int x) :
+        destinationNetwork(d),
+        nextHopRouter(n),
+        numberOfHops(x)
+    {}
+    
+    string destinationNetwork;
+    string nextHopRouter;
+    int numberOfHops;
+};
+
+class Router
+{
+public:
+    Router()
+    {
+        ;
+    }
+    
+    // Insert into table
+    void InsertIntoTable(string dest, string nextRouter, int num)
+    {
+        RoutingTableEntry entry(dest, nextRouter, num);
+        routingTable.push_back(entry);
+    }
+    
+    // Get an entry at index i
+    RoutingTableEntry getTableEntry(int i)
+    {
+        if (i >= routingTable.size()) RoutingTableEntry();
+    
+        return routingTable[i];
+    }
+    
+    
+    
+    
+private:
+    vector<RoutingTableEntry> routingTable;
+};
+
+
+
 
 int main(int argc, const char * argv[]) {
     
