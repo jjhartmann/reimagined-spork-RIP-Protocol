ef75538d3e7f496dc59394caaaad2cbebb20b2a3
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-11 00:29:41 -0800

    INIT: Routing Information Protocol Simulation

---
+++ RIP_Simulation/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  RIP_Simulation
+//
+//  Created by Jeremy Hartmann on 2016-03-10.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    // RIP Protocol Simulation
+    
+    
+    
+    
+    return 0;
+}
